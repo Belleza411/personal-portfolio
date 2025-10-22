e85afb1295b00108bc356f2e72bd147696104198
Belleza411 <aevanazelleb@gmail.com>
2025-10-23 11:51:54 +1300

    feat: Added navigation bar along with a hamburger menu for mobile devices

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import { projects } from "./data/projects.js";
 import { generateGlowingTech } from "./helpers/generateGlowingTech.js";
 import { generateProjects } from "./helpers/generateProjects.js";
+import { toggleHamburgerMenu } from "./helpers/toggleHamburgerMenu.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     generateProjects(projects);
     generateGlowingTech();
+    toggleHamburgerMenu();
 })
 
