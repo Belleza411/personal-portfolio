501d89af054bc58b969da120f21cd2aaaa157720
Belleza411 <aevanazelleb@gmail.com>
2025-10-25 11:54:24 +1300

    fix: Fix path mistakes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-import { generateGlowingTech } from "./helpers/generateGlowingTech.js";
 import { toggleHamburgerMenu } from "./helpers/toggleHamburgerMenu.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    if(window.location.pathname.endsWith('index.html')) {
+    const path = window.location.pathname;
+
+
+    if (path === "/" || path.endsWith("index.html")) {
         const { projects } = await import("./data/projects.js");
         const { generateProjects } = await import("./helpers/generateProjects.js");
+        const { generateGlowingTech } = await import("./helpers/generateGlowingTech.js");
         generateProjects(projects);
+        generateGlowingTech();
     }
-    generateGlowingTech();
     toggleHamburgerMenu();
 })
 
