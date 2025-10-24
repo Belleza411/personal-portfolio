4fdbd45e3000dcccb0392bfb842f7062a9acc473
Belleza411 <aevanazelleb@gmail.com>
2025-10-25 11:55:16 +1300

    refactor: Removed unecessary lines

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ import { toggleHamburgerMenu } from "./helpers/toggleHamburgerMenu.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     const path = window.location.pathname;
-
-
+    
     if (path === "/" || path.endsWith("index.html")) {
         const { projects } = await import("./data/projects.js");
         const { generateProjects } = await import("./helpers/generateProjects.js");
